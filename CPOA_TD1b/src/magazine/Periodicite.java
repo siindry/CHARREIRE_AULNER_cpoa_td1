d61a906fa58193518f05ba261acd25271608a40c
user <user@192.168.150.29>
2019-09-18 17:18:56 +0200

    add: ajout+supp à Periodicite

--- CPOA_TD1b/src/magazine/Periodicite.java
+++ CPOA_TD1b/src/magazine/Periodicite.java
@@ -1,31 +1,117 @@
 package magazine;
 import java.sql.*;
+import java.util.Scanner;
 
 public class Periodicite extends Connexion {
 	
 	//doit contenir les fonctions : insérer, supprimer, selectionner
 	
+	private String nom_periodicite;
+	
+	
+	public Periodicite(String nom_periodicite) {
+		this.nom_periodicite = nom_periodicite;
+	}
+	
+
+	public String getNom_periodicite() {
+		return nom_periodicite;
+	}
+
+	public void setNom_periodicite(String nom_periodicite) {
+		this.nom_periodicite = nom_periodicite;
+	}
+
+
+
+
+	public void choixPeriode() {
+		
+		
+		System.out.println("Que souhaitez-vous faire sur la table Periodicité: \n 1.Ajouter \n 2.Modifier "
+				+ "\n 3.Supprimer \n 4.Sélectionner");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String choix = sc.nextLine();
+		switch(choix) {
+		case "1": this.inserePeriode();
+			break;
+		//case 2: this.modifPeriode();
+			//break;
+		case "3": this.suppPeriode();
+			break;
+		//case 4: this.selectPeriode();
+			//break;
+		default: System.out.println("Entrée inconnue");
+		
+		}
+		
+		
+	}
+	
 	public void inserePeriode(){
+		
+		
 		Connexion connection = new Connexion();
 		Connection laConnexion = connection.creeConnexion();
 		
-		PreparedStatement req;
+		System.out.println("Quel type de periodicité souhaitez-vous ajouter ?");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String nom = sc.nextLine();
+		
 		try {
-			req = laConnexion.prepareStatement("insert into periodicite (id_periodicite, libelle) values(?, ?)",
+			PreparedStatement req = laConnexion.prepareStatement("insert into Periodicite (libelle) value(?)",
 					 Statement.RETURN_GENERATED_KEYS);
 			
-			req.setInt(1, 6);
-			req.setString(2, "Annuel");
+			req.setString(1, nom);
 			
 
-			req.executeQuery();
+			int i = req.executeUpdate();
+			System.out.println("ligne touché : " + i);
 			
 			
 		} catch (SQLException sqle) {
-			System.out.println("Pas connectÃ©" + sqle.getMessage());
+			System.out.println("Pas connecté" + sqle.getMessage());
 			
 		
 		}
+		
+		System.out.println("done");
+		
+		
+		}
+	
+	public void suppPeriode(){
+		
+		System.out.println("Quel type de periodicité souhaitez-vous supprimer ?");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String nom = sc.nextLine();
+		
+		Connexion connection = new Connexion();
+		Connection laConnexion = connection.creeConnexion();
+		
+		try {
+			PreparedStatement req = laConnexion.prepareStatement("delete from Periodicite where libelle=?");
+				
+			req.setString(1, nom); // 1 correspond au 1er para du where
+			
+
+			int i = req.executeUpdate();
+			System.out.println("ligne touché : " + i);
+			
+			
+		} catch (SQLException sqle) {
+			System.out.println("Pas connecté" + sqle.getMessage());
+			
+		
+		}
+		
+		System.out.println("done");
 		
 		
 		}
--- CPOA_TD1b/src/magazine/gestionM.java
+++ CPOA_TD1b/src/magazine/gestionM.java
@@ -1,11 +1,34 @@
 package magazine;
 
+import java.util.Scanner;
+
 public class gestionM {
 
 	public static void main(String[] args) {
-		System.out.println("yo");
+		System.out.println("Choisissez une table à éditer : \n 1. Periodicité \n 2.Revue \n "
+				+ "3.Client \n 4.Abonnement");
 		
-
+		Scanner sc = new Scanner(System.in);
+		
+		String choix = sc.nextLine();
+		
+		switch(choix) {
+		case "1": Periodicite p1 = new Periodicite ("");
+								p1.choixPeriode();
+			break;
+		/*case 2: choixRevue();
+			break;
+		case 3: choixClient();
+			break;
+		case 4: choixAbo();
+			break;*/
+		default: System.out.println("Entrée inconnue");
+		
+		}
+		
+		
+		
+		System.out.println("Bonsoir");
 	}
 
 }
